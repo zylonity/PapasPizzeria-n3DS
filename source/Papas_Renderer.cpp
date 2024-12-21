57bc6b10cbcc80ee1ac636cea671d4887f44f688
zylo_dev <70657892+zylonity@users.noreply.github.com>
2024-12-21 03:22:51 +0000

    enabled vsync

--- source/Papas_Renderer.cpp
+++ source/Papas_Renderer.cpp
@@ -36,7 +36,7 @@ PapasError Papas::Renderer::init(Papas::SceneManager* sceneManager) {
 	//Init the first scene
 	sceneManager->changeScene(new Papas::MainMenu());
 
-	gspWaitForVBlank();
+
 	return PAPAS_OK;
 }
 
@@ -81,7 +81,7 @@ PapasError Papas::Renderer::render(Papas::SceneManager* sceneManager) {
 #endif
 	
 
-
+	gspWaitForVBlank();
 	return PAPAS_OK;
 }
 
